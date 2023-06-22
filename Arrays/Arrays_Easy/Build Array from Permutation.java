9b039d504bfbd9dff627f5d85409fdaefcc369ac
Manoj S <134809278+manojscoder@users.noreply.github.com>
2023-06-22 23:43:57 +0530

    Create Build Array from Permutation.java

---
+++ Arrays/Arrays_Easy/Build Array from Permutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=nums[i]+(nums.length*(nums[nums[i]]%nums.length));
+        }
+        for(int i=0;i<nums.length;i++)
+            nums[i]/=nums.length;
+        return nums;
+    }
+}

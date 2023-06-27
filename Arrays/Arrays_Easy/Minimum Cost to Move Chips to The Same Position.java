76d36b6d71a464333efa0c25ecb02d92b4014df1
Manoj S <134809278+manojscoder@users.noreply.github.com>
2023-06-27 23:57:35 +0530

    Create Minimum Cost to Move Chips to The Same Position.java

---
+++ Arrays/Arrays_Easy/Minimum Cost to Move Chips to The Same Position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int odd=0,even=0;
+        for(int i:position)
+        {
+            if(i%2==0)
+                even++;
+            else
+                odd++;
+        }
+        return even>odd?odd:even;
+    }
+}

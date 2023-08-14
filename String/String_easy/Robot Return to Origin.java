ed901282d0b2b1b77e83c40f6bf5fc19e7b773b4
Manoj S <134809278+manojscoder@users.noreply.github.com>
2023-08-15 00:26:47 +0530

    Create Robot Return to Origin.java

---
+++ String/String_easy/Robot Return to Origin.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int UD=0,LR=0;
+        for(char move:moves.toCharArray())
+        {
+            if(move=='U') UD++;
+            if(move=='D') UD--;
+            if(move=='L') LR++;
+            if(move=='R') LR--;
+        }
+        return UD==0 && LR==0;
+    }
+}
